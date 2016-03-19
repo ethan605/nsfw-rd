dea56944ee537bc49fd5323486b86a8c9f89c81a
Ethan N <thanhnx.605@gmail.com>
2016-03-19 11:52:16 +0700

    Finalize Spiders::Instagram::Profile

--- lib/spiders/instagram/profile.rb
+++ lib/spiders/instagram/profile.rb
@@ -6,10 +6,24 @@ class Spiders::Instagram::Profile
   attr_accessor :images
 
   def initialize(args = {})
-    hash_profile = {}
-    hash_profile = args["hash_profile"] if args["hash_profile"] && args["hash_profile"].is_a?(Hash)
-    hash_profile = JSON.parse(File.read(args["json_profile"])) if args["json_profile"] && File.exists?(args["json_profile"])
+    profile = nil
+    profile = args[:hash_profile] if args[:hash_profile] && args[:hash_profile].is_a?(Hash)
 
-    hash_profile.each {|key, value| send(key + "=", value)}
+    if args[:screen_name] || args[:file_name]
+      json_file = Spiders::Instagram::Constants::FOLDER_NAME + "#{args[:screen_name]}-images.json" if args[:screen_name]
+      json_file = args[:file_name] if args[:file_name]
+      profile = JSON.parse(File.read(json_file)) if File.exists?(json_file)
+    end
+
+    if profile.nil? || profile.empty?
+      raise ArgumentError, "invalid argument: must be either a valid hash_profile, screen_name or file_name"
+    end
+
+    profile.each {|key, value| send(key.to_s + "=", value)}
+  end
+
+  def save_to_file(file_name = nil)
+    file_name = Spiders::Instagram::Constants::FOLDER_NAME + "#{self.screen_name}-images.test.json" unless file_name
+    File.write(file_name, JSON.pretty_generate(self.as_json))
   end
 end
