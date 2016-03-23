1b6b1c95beb0019c5c47f376a2332a57c6e4a906
Ethan N <thanhnx.605@gmail.com>
2016-03-23 21:07:33 +0700

    Finalize tests

--- app/models/content/image.rb
+++ app/models/content/image.rb
@@ -3,11 +3,11 @@ class Content::Image < ActiveRecord::Base
   has_many :compare_record_as_firsts, class_name: "Content::CompareRecord", foreign_key: "first_image_id"
   has_many :compare_record_as_seconds, class_name: "Content::CompareRecord", foreign_key: "second_image_id"
 
-  validates_associated :profile, :compare_records
+  validates_associated :profile, :compare_record_as_firsts, :compare_record_as_seconds
   validates :image_url, :presence => true, :uniqueness => true, :format => {:with => URI.regexp}
   validates_numericality_of :raw_average_score, :weighted_score
 
   def compare_records
-    Content::CompareRecord.where{(first_image_id == img13.id) | (second_image_id == img13.id)}
+    Content::CompareRecord.where{(first_image_id == self.id) | (second_image_id == self.id)}
   end
 end
